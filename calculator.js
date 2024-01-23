e255552e8b7ad33f301940b286117560448cae0b
SkyeYang001 <143848467+SkyeYang001@users.noreply.github.com>
2024-01-23 15:47:15 -0800

    Start
    1/23/24

---
+++ calculator.js
@@ -0,0 +1,43 @@
+//function () {}
+
+function entry() {
+
+    let data = prompt("Type Number", "");
+    if (data != )
+
+}
+
+
+
+function addition(num) {
+
+    let old = ``;
+    for (let add = ``;) {
+
+        old + add; 
+
+    }
+}
+
+/*function subtraction(num) {
+
+}
+
+function multiplication(num) {
+
+}
+
+function division(num) {
+
+}
+
+prompt()
+
+alert()*/
+
+
+//console.log(()); here
+
+console.log(addition(0));
+
+//module.exports = ; here
