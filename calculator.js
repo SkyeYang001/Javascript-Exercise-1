1ba81b98f01cf456f61d6209c2adab2730298c97
SkyeYang001 <143848467+SkyeYang001@users.noreply.github.com>
2024-01-25 15:32:53 -0800

    Revisions
    1/25/24

--- calculator.js
+++ calculator.js
@@ -1,20 +1,20 @@
 //function () {}
 
-function prompt(calculator) {
+function calculator() {
 
-    let = Num1 = prompt(`Type A Number`, ``);
+    let = Num1 = Number(prompt(`Type A Number`, ``));
 
-    let = Num2 = prompt(`Type A Number`, ``);
+    let = Num2 = Number(prompt(`Type A Number`, ``));
 
-    let = operator = prompt(`Type A Operator`, ``);
+    let = operator = Number(prompt(`Type A Operator`, ``));
 
-    if (!Number.isNaN(Num1 + Num2)) {
+    if (!Number.isNaN(Num1)) (!Number.isNaN(Num2)) ( + || - || * || / === operator){
 
-        document.getElementById("demo").innerHTML = Num1;
+        document.getElementById("Num1").innerHTML = Num1;
 
-        document.getElementById("demo").innerHTML = operator;
+        document.getElementById("operator").innerHTML = operator;
 
-        document.getElementById("demo").innerHTML = Num2;
+        document.getElementById("Num2").innerHTML = Num2;
 
         document.write(`=` + Num1 + operator + Num2);
 
